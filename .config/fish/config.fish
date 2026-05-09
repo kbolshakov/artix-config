bd3e4b70a564711a84a3cebd34528ceed8851671
kbolshakov <kbolsh.beng@gmail.com>
2026-05-09 15:35:41 -0400

    Update config.fish to include path.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-#fastfetch --logo-color-1 blue --color blue  # this is a test
+    # Commands to run in interactive sessions can go here
+    #fastfetch --logo-color-1 blue --color blue  # this is a test
+    fish_add_path /usr/local/sbin /usr/local/bin /usr/sbin/ /usr/bin /sbin /bin
 end
